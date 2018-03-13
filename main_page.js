13070cd400757b86e47519dd7d12e8dac495d702
Jacob Lyons <lyonsja@oregonstate.edu>
2018-03-12 19:39:38 -0700

    Implemented seller posting capability

---
+++ main_page.js
@@ -0,0 +1,70 @@
+		
+
+
+
+
+if(localStorage.getItem("new_element_toggle") == 1){
+
+	var image_url = localStorage.getItem("image_url");
+	var name = localStorage.getItem("name");
+	var price = localStorage.getItem("price");
+	var condition = localStorage.getItem("condition");
+	var access_code = localStorage.getItem("access_code");
+
+	createBook(image_url, name, price, condition, access_code);
+	//localStorage.setItem("new_element_toggle", 0);
+}
+
+function createBook(image_url, name, price, condition, access_code) {
+	var bookDiv = document.createElement('div');
+	bookDiv.classList.add('book');
+
+
+	var bookContentsDiv = document.createElement('div');
+	bookContentsDiv.classList.add('book-contents');
+	bookDiv.appendChild(bookContentsDiv);
+
+	var imageCont = document.createElement('div');
+	imageCont.classList.add('book-image-container');
+	bookContentsDiv.appendChild(imageCont);
+
+	var bookImage = document.createElement('img');
+	bookImage.src = image_url;
+	bookImage.alt = "book";
+	imageCont.appendChild(bookImage);
+
+	var bookInfoDiv = document.createElement('div');
+	bookInfoDiv.classList.add('book-info-container');
+	bookContentsDiv.appendChild(bookInfoDiv);
+
+	var bookLink = document.createElement('a');
+	bookLink.classList.add('book-name');
+	bookLink.href = '#';
+	bookLink.textContent = name;
+	bookInfoDiv.appendChild(bookLink);
+
+
+	var priceSpan = document.createElement('span');
+	priceSpan.classList.add("book-price");
+	priceSpan.textContent = '$' + price;
+	bookInfoDiv.appendChild(priceSpan);
+
+
+	var condSpan = document.createElement('span');
+	condSpan.classList.add("book-condition");
+	condSpan.textContent = "Condition: " + condition;
+	bookInfoDiv.appendChild(condSpan);
+
+	var codeSpan = document.createElement('span');
+	codeSpan.classList.add("book-access-code");
+	codeSpan.textContent = "Access Code: " + access_code;
+	bookInfoDiv.appendChild(codeSpan);
+
+	const insertLoc = document.getElementById("Main-div");
+	//alert(document.getElementById("Main-div"));
+
+
+	insertLoc.appendChild(bookDiv);
+
+
+}
---
+++ seller_script.js
@@ -0,0 +1,35 @@
+
+
+
+
+
+if(document.getElementById('add_book')){
+	document.getElementById("add_book").addEventListener("click", function(){
+		var image_url = document.getElementById('Book-Image-Input').value;
+		var name = document.getElementById('Book-Name-Input').value;
+		var price = document.getElementById('Book-Price-Input').value;
+		var condition = document.getElementById('Book-Condition-Input').value;
+		var access_code = document.getElementById('codeChoice1').value;
+
+		if(!access_code) {
+			access_code = document.getElementById('codeChoice2').value;
+		}
+
+
+		//save values to local storage
+
+		localStorage.setItem("image_url", image_url);
+		localStorage.setItem("name", name);
+		localStorage.setItem("price", price);
+		localStorage.setItem("condition", condition);
+		localStorage.setItem("access_code", access_code);
+
+		localStorage.setItem("new_element_toggle", 1);
+		document.location.href = "./index.html";
+
+	});
+}
+
+
+
+
