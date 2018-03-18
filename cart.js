b5363a3707dfec4194607e64d95dc6a95e9ba98a
Jacob Lyons <lyonsja@oregonstate.edu>
2018-03-17 18:41:44 -0700

    Fixed book removal to be persistent, fixed navbar links

--- cart.js
+++ cart.js
@@ -1,6 +1,10 @@
 
 document.getElementById("book1-remove").addEventListener("click", function(){
 	document.getElementById("book1-row").style.display = 'none';
-
+	localStorage.setItem("removed_book", 1);
 
 });
+
+if(localStorage.getItem("removed_book")){
+		document.getElementById("book1-row").style.display = 'none';
+}
